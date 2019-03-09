a4d1117ab3c755eba9e52cbaa96ae6fbb09fd90c
Noone <ahlijun1116@outlook.com>
2019-03-09 23:55:15 +0800

    add merge_sort

--- merge_sort.py
+++ merge_sort.py
@@ -4,14 +4,13 @@ def merge_sort(collection):
         midpoint = length // 2
         left_half = merge_sort(collection[:midpoint])
         right_half = merge_sort(collection[midpoint:])
-        i = 0
-        j = 0
-        k = 0
+        i, j, k = 0, 0, 0
         left_length = len(left_half)
         right_length = len(right_half)
         while i < left_length and j < right_length:
             if left_half[i] < right_half[j]:
                 collection[k] = left_half[i]
+                # print(collection)
                 i += 1
             else:
                 collection[k] = right_half[j]
@@ -22,15 +21,12 @@ def merge_sort(collection):
             collection[k] = left_half[i]
             i += 1
             k += 1
-
         while j < right_length:
             collection[k] = right_half[j]
             j += 1
             k += 1
-
     return collection
 
 
-a = [1, 2, 345, 6, 6, 7, 9, 90]
-b = merge_sort(a)
-print(b)
+list=[5,4,2,1,1,3,5,7]
+print(merge_sort(list))
